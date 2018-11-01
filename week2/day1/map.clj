4069af1e9e16bd6bc055085423a52d14efe77677
hidehalo <tianchen_cc@yeah.net>
2018-11-01 08:38:30 +0800

    fix: string + int error

--- week2/day1/map.clj
+++ week2/day1/map.clj
@@ -1,8 +1,8 @@
 (def counts {"apple" 2 "orange" 1})
-(print counts)
+(print counts "\n")
 ; FIXME
-(print (+ (get counts "apple" 0) "\n"))
-(print (get counts "banana" 0))
+(print (get counts "apple" 0) "\n")
+(print (get counts "banana" 0) "\n")
 (def counts (assoc counts "banana" 1))
 (def counts (assoc counts "apple" 3))
-(print counts)
+(print counts "\n")
